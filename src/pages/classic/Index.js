d87535c433098044d97df073768ab43fd0487ad7
HsuSheng <yso9197ce@gmail.com>
2022-03-05 00:40:36 +0800

    Sheng: 新增tag資料夾及tag圖案、classic切版

--- src/pages/classic/Index.js
+++ src/pages/classic/Index.js
@@ -1,4 +1,5 @@
 import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
+import './index.css';
 
 function Home() {
   return (
@@ -9,6 +10,14 @@ function Home() {
         <div style={{ width: '100%' }}>
           <Title title={'Classic'} />
           <br />
+
+          <div className='category-box'>
+            <div className='en-title-14-10'>SUSHI</div>
+            <div className='en-title-14-10'>DESSERT</div>
+            <div className='en-title-14-10'>PACKAGE</div>
+          </div>
+
+
 
           <Footer />
         </div>
