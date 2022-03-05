30c366b279e96f1e98db6a4ead1c452077d30ed8
Joe Shih <injoe1001@gmail.com>
2022-03-05 23:50:39 +0800

    Joe Login頁面切版 / 新增mem/img資料夾

--- src/pages/member/Login.js
+++ src/pages/member/Login.js
@@ -2,18 +2,33 @@ import styled from 'styled-components';
 import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
 import { useState } from 'react';
 
+const LoginBody = styled.body`
+  background: #212121;
+`;
+const Slogan = styled.p`
+  color: #f7f6f3;
+  text-align: center;
+`;
 const LoginArea = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: end;
-`;
-const LoginButton = styled.button`
-  margin-top: 2%;
+  justify-content: space-between;
+  height: 100%;
 `;
 
-const handleSubmit = (e) => {
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const InputArea = styled.div`
+  margin-top: 25%;
+`;
+const LoginAreaImg = styled.div`
+  height: 100vh;
+`;
+
+const handleSubmit = e => {
   e.preventDefault();
-}
+};
 
 function Login() {
   const [account, setAccount] = useState('');
@@ -21,42 +36,51 @@ function Login() {
 
   return (
     <>
-      <body>
-        <Header />
-        <div style={{ display: 'flex' }}>
+      <LoginBody>
+        {/* <Header /> */}
+        <div style={{ display: 'flex', height: '100vh' }}>
           <AsideLeft />
           <div style={{ width: '100%' }}>
-            <Title title={''} />
-            <br />
-
+            {/* <Title title={''} /> */}
             <LoginArea>
-              <p>Hello, My Friend</p>
-              <form onSubmit={handleSubmit}>
-                <p>帳號</p>
-                <input
-                  type="text"
-                  value={account}
-                  onChange={e => {
-                    setAccount(e.target.value);
-                  }}
-                />
-                <p>密碼</p>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={e => {
-                    setPassword(e.target.value);
-                  }}
-                />
-                <LoginButton>登入</LoginButton>
-              </form>
+              <LoginAreaImg>
+                <img
+                  src={require('../../imgs/mem/LoginImg.png')}
+                  style={{ width: '100%', height: '100%' }}
+                ></img>
+              </LoginAreaImg>
+              <InputArea>
+                <Slogan className="en-title-24">
+                  Hello, <br />
+                  My Friend
+                </Slogan>
+                <LoginForm onSubmit={handleSubmit}>
+                  <p className="ch-cont-14">帳號</p>
+                  <input
+                    type="text"
+                    value={account}
+                    onChange={e => {
+                      setAccount(e.target.value);
+                    }}
+                  />
+                  <p className="ch-cont-14">密碼</p>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={e => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                  <button>登入</button>
+                </LoginForm>
+              </InputArea>
             </LoginArea>
 
-            <Footer />
+            {/* <Footer /> */}
           </div>
           <AsideRight />
         </div>
-      </body>
+      </LoginBody>
     </>
   );
 }
