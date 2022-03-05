9e434821ecad4f5c3e6813526d493b70174c036f
Joe Shih <injoe1001@gmail.com>
2022-03-05 11:09:26 +0800

    Joe 登入頁功能小幅更新

--- src/pages/member/Login.js
+++ src/pages/member/Login.js
@@ -1,42 +1,58 @@
 import styled from 'styled-components';
 import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
+import { useState } from 'react';
 
 const LoginArea = styled.div`
   display: flex;
   flex-direction: column;
-  position: fixed;
-  right: 180px;
-  top: 30%;
+  align-items: end;
 `;
-const LoginButton = styled.button``;
+const LoginButton = styled.button`
+  margin-top: 2%;
+`;
 
 function Login() {
+  const [account, setAccount] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
-      <Header />
-      <div style={{ display: 'flex' }}>
-        <AsideLeft />
-        <div style={{ width: '100%' }}>
-          <Title title={'Classic'} />
-          <br />
+      <body>
+        <Header />
+        <div style={{ display: 'flex' }}>
+          <AsideLeft />
+          <div style={{ width: '100%' }}>
+            <Title title={''} />
+            <br />
 
-          <LoginArea>
-            <p>Hello, My Friend</p>
-            <div>
-              <p>帳號</p>
-              <input type="text" />
-            </div>
-            <div>
-              <p>密碼</p>
-              <input type="password" />
-            </div>
-            <LoginButton>登入</LoginButton>
-          </LoginArea>
+            <LoginArea>
+              <p>Hello, My Friend</p>
+              <form>
+                <p>帳號</p>
+                <input
+                  type="text"
+                  value={account}
+                  onChange={e => {
+                    setAccount(e.target.value);
+                  }}
+                />
+                <p>密碼</p>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={e => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                <LoginButton>登入</LoginButton>
+              </form>
+            </LoginArea>
 
-          <Footer />
+            <Footer />
+          </div>
+          <AsideRight />
         </div>
-        <AsideRight />
-      </div>
+      </body>
     </>
   );
 }
