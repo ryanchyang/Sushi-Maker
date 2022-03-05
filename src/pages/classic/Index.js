29f8352c7151915f4e0fb2674a267d2cf8f94ffa
HsuSheng <yso9197ce@gmail.com>
2022-03-05 17:35:15 +0800

    Sheng: classic切版

--- src/pages/classic/Index.js
+++ src/pages/classic/Index.js
@@ -1,7 +1,9 @@
 import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
 import './index.css';
+import { ReactComponent as Discount } from '../../imgs/tags/discount_25.svg';
+import { ReactComponent as Cart } from '../../imgs/tags/add_cart.svg';
 
-function Home() {
+function Index() {
   return (
     <>
       <Header />
@@ -11,11 +13,106 @@ function Home() {
           <Title title={'Classic'} />
           <br />
 
+          {/* category-tag */}
           <div className='category-box'>
             <div className='en-title-14-10'>SUSHI</div>
             <div className='en-title-14-10'>DESSERT</div>
             <div className='en-title-14-10'>PACKAGE</div>
           </div>
+
+          <div className='prod-list'>
+            {/* product-card */}
+            <div className='prod-card'>
+              <div className='prod-img-box'>
+                <div className='discount-tag'><Discount /></div>              
+                <img src={require('./../../imgs/temp/classic-pro1.png')} alt="product-image" />
+              </div>
+
+              <div className='prod-name-ch ch-title-22'>鮭魚壽司</div>
+              <div className='prod-name-en en-title-14-5'>Salmon Sushi</div> 
+              
+              <div className='prod-price-special'>
+                <div className='original-price ch-cont-14'>NT_60</div>
+                <div className='special-price ch-cont-16'>NT_50</div>
+              </div>
+
+              <div className='prod-price-no-discount'>
+                <div className='no-discount ch-cont-16'>NT_60</div>
+              </div> 
+
+              <div className="select-add-cart">
+                <div className='select-count'>
+                  <button>-</button>
+                  <input type="number" value={1} />
+                  <button>+</button>
+                </div>
+                <div className='discount-tag1'>
+                  <Cart />
+                </div>
+              </div>
+            </div>
+
+            <div className='prod-card'>
+              <div className='prod-img-box'>
+                <div className='discount-tag'><Discount /></div>              
+                <img src={require('./../../imgs/temp/classic-pro1.png')} alt="product-image" />
+              </div>
+
+              <div className='prod-name-ch ch-title-22'>鮭魚壽司</div>
+              <div className='prod-name-en en-title-14-5'>Salmon Sushi</div> 
+              
+              <div className='prod-price-special'>
+                <div className='original-price ch-cont-14'>NT_60</div>
+                <div className='special-price ch-cont-16'>NT_50</div>
+              </div>
+
+              <div className='prod-price-no-discount'>
+                <div className='no-discount ch-cont-16'>NT_60</div>
+              </div> 
+
+              <div className="select-add-cart">
+                <div className='select-count'>
+                  <button>-</button>
+                  <input type="number" value={1} />
+                  <button>+</button>
+                </div>
+                <div className='discount-tag1'>
+                  <Cart />
+                </div>
+              </div>
+            </div>
+
+            <div className='prod-card'>
+              <div className='prod-img-box'>
+                <div className='discount-tag'><Discount /></div>              
+                <img src={require('./../../imgs/temp/classic-pro1.png')} alt="product-image" />
+              </div>
+
+              <div className='prod-name-ch ch-title-22'>鮭魚壽司</div>
+              <div className='prod-name-en en-title-14-5'>Salmon Sushi</div> 
+              
+              <div className='prod-price-special'>
+                <div className='original-price ch-cont-14'>NT_60</div>
+                <div className='special-price ch-cont-16'>NT_50</div>
+              </div>
+
+              <div className='prod-price-no-discount'>
+                <div className='no-discount ch-cont-16'>NT_60</div>
+              </div> 
+
+              <div className="select-add-cart">
+                <div className='select-count'>
+                  <button>-</button>
+                  <input type="number" value={1} />
+                  <button>+</button>
+                </div>
+                <div className='discount-tag1'>
+                  <Cart />
+                </div>
+              </div>
+            </div>
+          </div>
+          
 
 
 
@@ -27,4 +124,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Index;
