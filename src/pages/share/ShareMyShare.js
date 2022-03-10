38adc07b850a52114a294d4f601fb0f0f6dcff62
Ryan Yang <ryanyang83@gmail.com>
2022-03-10 12:02:02 +0800

    Ryan items rwd

--- src/pages/share/ShareMyShare.js
+++ src/pages/share/ShareMyShare.js
@@ -1,5 +1,35 @@
+import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
+
+import Masonry from './components/Masonry';
+import { ReactComponent as Delete } from '../../imgs/delete-lg.svg';
+
 function ShareMyShare() {
-  return <>ShareMyShare</>;
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
+        <AsideLeft />
+        <div style={{ width: '100%' }}>
+          <Title title={'Share'} />
+          <div className={`mycontainer`}>
+            <div className="row">
+              <div className="col">
+                <div className="d-flex justify-content-between">
+                  <p className="mytitle en-title-14-10">
+                    HOME / SHARE / MY-SHARE
+                  </p>
+                  <div className="d-flex align-items-center">
+                    <Delete className="mx-4 p-2" />
+                  </div>
+                </div>
+                <div className="bigprofile">123</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <AsideRight />
+      </div>
+    </>
+  );
 }
 
 export default ShareMyShare;
