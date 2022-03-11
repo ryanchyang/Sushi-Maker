aee0548a2c8092340fa8ac4e2801650206a80e7b
Joe Shih <injoe1001@gmail.com>
2022-03-11 11:35:49 +0800

    Joe 歷史訂單切版

--- src/pages/member/IndexHistoryOrder.js
+++ src/pages/member/IndexHistoryOrder.js
@@ -6,8 +6,13 @@ import {
   Footer,
 } from './memLayout/LayoutLight';
 import './index.scss';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function indexHistoryOrder() {
+function IndexHistoryOrder() {
   return (
     <>
       <Header />
@@ -72,6 +77,50 @@ function indexHistoryOrder() {
               </div>
             </div>
             {/* 以上不動 */}
+
+            <div className="mycontainer orderDetailTB">
+              <div className="row orderDetailRow">
+                <table className="table col-md-20">
+                  <thead>
+                    <tr>
+                      <th scope="col">訂單編號</th>
+                      <th scope="col">購買日期</th>
+                      <th scope="col">訂單狀態</th>
+                      <th scope="col">總價</th>
+                      <th scope="col">印製時間</th>
+                      <th scope="col"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>#0000001</td>
+                      <td>2022/4/30</td>
+                      <td>已出貨</td>
+                      <td>NT$ 1000 元</td>
+                      <td>30 分鐘</td>
+                      <td>交易明細</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <Accordion className="col-md-20">
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography sx={{ width: '90%', flexShrink: 0 }}>
+                      交易明細
+                    </Typography>
+                    <Typography sx={{ width: '10%', flexShrink: 0 }}>
+                      數量
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails className="w-100">
+                    <Typography>{/* 商品詳細資訊 */}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </div>
           </div>
           <Footer />
         </div>
@@ -81,4 +130,4 @@ function indexHistoryOrder() {
   );
 }
 
-export default indexHistoryOrder;
+export default IndexHistoryOrder;
