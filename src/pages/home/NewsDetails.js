16f2b4b9bb4248a03c89d95ab1dea676e9c27184
Sandy Fu <fusandy8260@gmail.com>
2022-03-12 23:07:32 +0800

    Sandy : add home NewsDetails.js

--- src/pages/home/NewsDetails.js
+++ src/pages/home/NewsDetails.js
@@ -1,5 +1,5 @@
 import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
-
+import './news-details.scss';
 function NewsDetails() {
   return (
     <>
@@ -21,6 +21,52 @@ function NewsDetails() {
                   </div>
                   <div className="filter">
                     <img src="/img/home/filter-icon.svg" alt="filter" />
+                  </div>
+                </div>
+              </div>
+
+              {/* mobile latest-news-content */}
+              <div class="mobile-news-detail d-sm-none">
+                <div class="ch-title-18 news-title">
+                  春天來了! 羊羹櫻花凍口味新上市!
+                </div>
+                <div class="news-date-tag">
+                  <div class="en-cont-14 news-date">2022.05.06</div>
+                  <div class="ch-cont-14 news-tag">新品上市</div>
+                </div>
+                <div class="news-img">
+                  <img src="/img/home/news/new-cherry-blossom.png" alt="news" />
+                </div>
+                <div class="ch-cont-16 news-content">
+                  充滿粉紅氣息的春天終於來了! <br />
+                  PRIMEAL推出粉色的羊羹櫻花凍口味壽司，清爽口感搭配淡淡的櫻花香，給您不一樣的2022春天!
+                </div>
+                <div class="ch-cont-14 news-warning">
+                  ※ 商品均以實體成品為主，圖片僅供參考
+                </div>
+              </div>
+            </div>
+
+            {/* pc latest-news-content */}
+            <div className="mycontainer d-none d-sm-block">
+              <div class="pc-news-detail d-flex justify-content-between">
+                <div class="pc-news-img">
+                  <img src="/img/home/news/new-cherry-blossom.png" alt="news" />
+                </div>
+                <div className="pc-news-content">
+                  <div class="ch-title-18 pc-news-title">
+                    春天來了! 羊羹櫻花凍口味新上市!
+                  </div>
+                  <div class="pc-news-date-tag">
+                    <div class="en-cont-14 pc-news-date">2022.05.06</div>
+                    <div class="ch-cont-14 pc-news-tag">新品上市</div>
+                  </div>
+                  <div class="ch-cont-16 pc-new-text">
+                    充滿粉紅氣息的春天終於來了! <br />
+                    PRIMEAL推出粉色的羊羹櫻花凍口味壽司，清爽口感搭配淡淡的櫻花香，給您不一樣的2022春天!
+                  </div>
+                  <div class="ch-cont-14 pc-news-warning">
+                    ※ 商品均以實體成品為主，圖片僅供參考
                   </div>
                 </div>
               </div>
