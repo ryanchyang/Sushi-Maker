32884a1be3b0122d1084ed9495fd2434982671d0
Sandy Fu <fusandy8260@gmail.com>
2022-03-13 14:32:41 +0800

    Sandy : 新增 NewsFilter.js EvntsFilter.js的下拉控制面板

--- src/pages/home/components/EvntsFilter.js
+++ src/pages/home/components/EvntsFilter.js
@@ -1,5 +1,10 @@
+import { Collapse } from 'react-bootstrap';
+import { useState } from 'react';
 import './fitler.scss';
 function EvntsFilter() {
+  const [dateOpen, setDateOpen] = useState(false);
+  const [cateOpen, setCateOpen] = useState(false);
+  const [statusOpen, setStatusOpen] = useState(false);
   return (
     <>
       {/* clean or cancel filter */}
@@ -21,23 +26,36 @@ function EvntsFilter() {
               <div className="diamond"></div>
               <div className="by-date-text en-title-18">By DATE</div>
             </div>
-            <div className="down-arrow">
-              <img src="/img/home/downarrow.svg" alt="down" />
+            <div
+              className="down-arrow"
+              onClick={() => setDateOpen(!dateOpen)}
+              aria-controls="example-collapse-text"
+              aria-expanded={dateOpen}
+            >
+              {dateOpen ? (
+                <img src="/img/home/uparrow.svg" alt="up" />
+              ) : (
+                <img src="/img/home/downarrow.svg" alt="down" />
+              )}
             </div>
           </div>
-          <div className="by-date-input">
-            <input
-              type="text"
-              placeholder="YYYY/MM/DD/"
-              onfocus="(this.type='date')"
-            ></input>
-            <div className="by-date-dash-line"></div>
-            <input
-              type="text"
-              placeholder="YYYY/MM/DD/"
-              onfocus="(this.type='date')"
-            ></input>
-          </div>
+          <Collapse in={dateOpen}>
+            <div className="d-sm-block">
+              <div className="by-date-input">
+                <input
+                  type="text"
+                  placeholder="YYYY/MM/DD/"
+                  onfocus="(this.type='date')"
+                ></input>
+                <div className="by-date-dash-line"></div>
+                <input
+                  type="text"
+                  placeholder="YYYY/MM/DD/"
+                  onfocus="(this.type='date')"
+                ></input>
+              </div>
+            </div>
+          </Collapse>
         </div>
 
         {/* by category */}
@@ -47,17 +65,30 @@ function EvntsFilter() {
               <div className="diamond"></div>
               <div className="by-date-text en-title-18">By EVENT CATEGORY</div>
             </div>
-            <div className="down-arrow">
-              <img src="/img/home/downarrow.svg" alt="down" />
+            <div
+              className="down-arrow"
+              onClick={() => setCateOpen(!cateOpen)}
+              aria-controls="example-collapse-text"
+              aria-expanded={cateOpen}
+            >
+              {cateOpen ? (
+                <img src="/img/home/uparrow.svg" alt="up" />
+              ) : (
+                <img src="/img/home/downarrow.svg" alt="down" />
+              )}
             </div>
           </div>
-          <div className="category-tag-box">
-            <div className="d-flex flex-wrap">
-              <div className="category-tag ch-title-14">講座活動</div>
-              <div className="category-tag ch-title-14">親子活動</div>
-              <div className="category-tag ch-title-14">品牌推廣</div>
+          <Collapse in={cateOpen}>
+            <div className="d-sm-block">
+              <div className="category-tag-box">
+                <div className="d-flex flex-wrap">
+                  <div className="category-tag ch-title-14">講座活動</div>
+                  <div className="category-tag ch-title-14">親子活動</div>
+                  <div className="category-tag ch-title-14">品牌推廣</div>
+                </div>
+              </div>
             </div>
-          </div>
+          </Collapse>
         </div>
 
         {/* by status */}
@@ -67,18 +98,31 @@ function EvntsFilter() {
               <div className="diamond"></div>
               <div className="by-date-text en-title-18">By STATUS</div>
             </div>
-            <div className="down-arrow">
-              <img src="/img/home/downarrow.svg" alt="down" />
+            <div
+              className="down-arrow"
+              onClick={() => setStatusOpen(!statusOpen)}
+              aria-controls="example-collapse-text"
+              aria-expanded={statusOpen}
+            >
+              {statusOpen ? (
+                <img src="/img/home/uparrow.svg" alt="up" />
+              ) : (
+                <img src="/img/home/downarrow.svg" alt="down" />
+              )}
             </div>
           </div>
-          <div className="category-tag-box">
-            <div className="d-flex flex-wrap">
-              <div className="category-tag ch-title-14">熱烈報名中</div>
-              <div className="category-tag ch-title-14">即將額滿</div>
-              <div className="category-tag ch-title-14">報名額滿</div>
-              <div className="category-tag ch-title-14">報名截止</div>
+          <Collapse in={statusOpen}>
+            <div className="d-sm-block">
+              <div className="category-tag-box">
+                <div className="d-flex flex-wrap">
+                  <div className="category-tag ch-title-14">熱烈報名中</div>
+                  <div className="category-tag ch-title-14">即將額滿</div>
+                  <div className="category-tag ch-title-14">報名額滿</div>
+                  <div className="category-tag ch-title-14">報名截止</div>
+                </div>
+              </div>
             </div>
-          </div>
+          </Collapse>
         </div>
 
         <div className="send-filter-btn-box">
--- src/pages/home/components/NewsFilter.js
+++ src/pages/home/components/NewsFilter.js
@@ -1,5 +1,10 @@
+import { Collapse } from 'react-bootstrap';
+import { useState } from 'react';
 import './fitler.scss';
-function NewsFilter(props) {
+function NewsFilter() {
+  const [dateOpen, setDateOpen] = useState(false);
+  const [cateOpen, setCateOpen] = useState(false);
+
   return (
     <>
       {/* clean or cancel filter */}
@@ -12,7 +17,7 @@ function NewsFilter(props) {
           <span>X</span>
         </div>
       </div>
-
+      .
       <form name="newsFilter">
         {/* by date */}
         <div className="by-date">
@@ -21,23 +26,37 @@ function NewsFilter(props) {
               <div className="diamond"></div>
               <div className="by-date-text en-title-18">By DATE</div>
             </div>
-            <div className="down-arrow">
-              <img src="/img/home/downarrow.svg" alt="down" />
+            <div
+              className="down-arrow"
+              onClick={() => setDateOpen(!dateOpen)}
+              aria-controls="example-collapse-text"
+              aria-expanded={dateOpen}
+            >
+              {/* {console.log('open:', open)} */}
+              {dateOpen ? (
+                <img src="/img/home/uparrow.svg" alt="up" />
+              ) : (
+                <img src="/img/home/downarrow.svg" alt="down" />
+              )}
             </div>
           </div>
-          <div className="by-date-input">
-            <input
-              type="text"
-              placeholder="YYYY/MM/DD/"
-              onfocus="(this.type='date')"
-            ></input>
-            <div className="by-date-dash-line"></div>
-            <input
-              type="text"
-              placeholder="YYYY/MM/DD/"
-              onfocus="(this.type='date')"
-            ></input>
-          </div>
+          <Collapse in={dateOpen}>
+            <div className="d-sm-block">
+              <div className="by-date-input">
+                <input
+                  type="text"
+                  placeholder="YYYY/MM/DD/"
+                  onfocus="(this.type='date')"
+                ></input>
+                <div className="by-date-dash-line"></div>
+                <input
+                  type="text"
+                  placeholder="YYYY/MM/DD/"
+                  onfocus="(this.type='date')"
+                ></input>
+              </div>
+            </div>
+          </Collapse>
         </div>
 
         {/* by category */}
@@ -47,18 +66,31 @@ function NewsFilter(props) {
               <div className="diamond"></div>
               <div className="by-date-text en-title-18">By NEWS CATEGORY</div>
             </div>
-            <div className="down-arrow">
-              <img src="/img/home/downarrow.svg" alt="down" />
+            <div
+              className="down-arrow"
+              onClick={() => setCateOpen(!cateOpen)}
+              aria-controls="example-collapse-text"
+              aria-expanded={cateOpen}
+            >
+              {cateOpen ? (
+                <img src="/img/home/uparrow.svg" alt="up" />
+              ) : (
+                <img src="/img/home/downarrow.svg" alt="down" />
+              )}
             </div>
           </div>
-          <div className="category-tag-box">
-            <div className="d-flex flex-wrap">
-              <div className="category-tag ch-title-14">新品上市</div>
-              <div className="category-tag ch-title-14">快閃特價</div>
-              <div className="category-tag ch-title-14">促銷特賣</div>
-              <div className="category-tag ch-title-14">會員公告</div>
+          <Collapse in={cateOpen}>
+            <div className="d-sm-block">
+              <div className="category-tag-box">
+                <div className="d-flex flex-wrap">
+                  <div className="category-tag ch-title-14">新品上市</div>
+                  <div className="category-tag ch-title-14">快閃特價</div>
+                  <div className="category-tag ch-title-14">促銷特賣</div>
+                  <div className="category-tag ch-title-14">會員公告</div>
+                </div>
+              </div>
             </div>
-          </div>
+          </Collapse>
         </div>
 
         <div className="send-filter-btn-box">
