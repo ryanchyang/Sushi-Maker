9c60da90ae3e2bef39a44005181f35d8def67c0e
kris <kris04090513@gmail.com>
2022-03-14 16:16:26 +0800

    Kris map 光箱

---
+++ src/pages/cart/StepMap.js
@@ -0,0 +1,183 @@
+// StepMap.js 選店地址
+import React, { useState } from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// import StoreData from './data/store.json';
+
+//  點地圖會可以抓到你目前所在的位置
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
+
+// 資料位置
+var arrCoordinates = [
+  [25.032, 121.539],
+  [25.022, 121.543],
+  [25.034, 121.537],
+];
+// mark icon 圖樣大小
+const icon = L.icon({
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+  iconUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png',
+});
+
+function MultipleMarkers() {
+  return arrCoordinates.map((coordinates, index) => {
+    // console.log('v', v);
+    return (
+      <Marker key={index} position={coordinates} icon={icon}>
+        {/* <Popup>{v.storeName}門市</Popup> */}
+      </Marker>
+    );
+  });
+}
+
+function StepMap() {
+  // const [store, setStore] = useState([]);
+  // const position = [51.505, -0.09];
+  // ---------------
+  // let map = L.map('map').setView([25.03, 121.54], 13); //初始中心點
+
+  // var Jawg_Sunny = L.tileLayer(
+  //   'https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}',
+  //   {
+  //     attribution:
+  //       '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  //     minZoom: 0,
+  //     maxZoom: 22,
+  //     subdomains: 'abcd',
+  //     accessToken:
+  //       'YR1KwDGBWLiThGFOeyYWRjm7p8MXSWg7PQGaqGXNokIJNZt7xFwLEBYzf1DhJ3t7',
+  //   }
+  // );
+
+  // Jawg_Sunny.addTo(map);
+
+  // var coordinateArray = [
+  //   { coordinate: [25.02, 121.535], storeName: '讚讚門市' }, //門市資料
+  //   { coordinate: [25.049187, 121.566344], storeName: '棒棒門市' },
+  //   { coordinate: [25.056714, 121.560524], storeName: '鳩鳩門市' },
+  //   { coordinate: [25.05639, 121.548287], storeName: '上弘門市' },
+  //   { coordinate: [25.04, 121.555], storeName: '寰寰門市' },
+  // ];
+
+  // coordinateArray.forEach(ele => {
+  //   //將門市資料綁到地標標誌上
+  //   L.marker(ele['coordinate'], {
+  //     title: ele['storeName'],
+  //     opacity: 1.0,
+  //   })
+  //     // .addTo(map)
+  //     .bindPopup(`${ele['storeName']}`)
+  //     .on('click', showStore);
+  // });
+
+  // function showStore(e) {
+  //   var storeName = e.target.getPopup().getContent();
+  //   document.querySelector('.store').innerHTML = storeName;
+  // }
+  // StoreData.map((v, index) => {
+  return (
+    <>
+      <form>
+        <div className="form-row d-flex justify-content-between px-0">
+          <div className="form-group col">
+            <label htmlFor="inputArea">縣市 </label>
+            <select id="inputArea" className="form-control ">
+              <option>台北 </option>
+              {/* <option>{v.storeCity} </option> */}
+            </select>
+          </div>
+          <div className="form-group col">
+            <label htmlFor="inputState">行政區 </label>
+            <select id="inputState" className="form-control ">
+              <option selected>Choose...</option>
+              <option>大安區</option>
+              <option>信義區</option>
+              <option>中正區</option>
+
+              {/* <option>{v.storeTown}</option> */}
+            </select>
+          </div>
+        </div>
+        <div className="form-group col px-0">
+          <label htmlFor="inputStore">門市</label>
+          <select id="inputStore" className="form-control">
+            <option selected>Choose...</option>
+            <option selected>12321門市</option>
+            <option selected>22231門市</option>
+
+            {/* <option>{v.storeName}門市</option> */}
+          </select>
+        </div>
+        <div
+          className="store-map"
+          id="map"
+          style={
+            {
+              // width: ' 100 %',
+              // height: '600px',
+              // backgroundColor: '#c4c4c4',
+            }
+          }
+        >
+          <MapContainer
+            center={{ lat: 25.03, lng: 121.54 }}
+            zoom={13}
+            style={{ height: '400px' }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <LocationMarker />
+            <Marker
+              position={[25.032, 121.539]}
+              // icon={greenIcon}
+              eventHandlers={{
+                click: () => {
+                  console.log('marker01');
+                  <Popup>Hello world</Popup>;
+                },
+              }}
+            />
+            <MultipleMarkers />
+          </MapContainer>
+        </div>
+        <div className="store-box" style={{ fontSize: '1.6rem' }}>
+          <i className="fas fa-shipping-fast"></i>&nbsp;請選擇您的印製取貨門市 :
+          <span className="store"></span>
+        </div>
+      </form>
+    </>
+  );
+  // });
+}
+export default StepMap;
--- src/pages/cart/StepTwo.js
+++ src/pages/cart/StepTwo.js
@@ -4,8 +4,47 @@ import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
 
 import CartSum from './CartSum';
 import CartDetail from './CartDetial';
+
+import React, { useState } from 'react';
+import StepMap from './StepMap';
+import { Button, Modal } from 'react-bootstrap';
+
+
 // TODO: A  改 LINK
 function StepTwo() {
+  // 套餐光箱
+  const [map, setMap] = useState(false);
+  const handleClose = () => setMap(false);
+  const handleShow = () => setMap(true);
+
+  const modal = (
+    <Modal show={map} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title className="en-cont-30">套餐說明</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <>
+          <StepMap />
+        </>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          className="btn btn-sm btn-primary primeal-btn-sm mx-5"
+          onClick={handleClose}
+        >
+          Close
+        </Button>
+        <Button
+          variant="btn btn-sm btn-primary primeal-btn-sm mx-5"
+          onClick={handleClose}
+        >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+
   return (
     <>
       <Header />
@@ -14,7 +53,7 @@ function StepTwo() {
         <div style={{ width: '100%' }}>
           <Title title={'Shipping Info'} />
           <br />
-          <div className="row"></div>
+          {/* <div className="row"></div> */}
           <div className="mycontainer cart">
             <div className="bread">HOME/CART</div>
             {/* 訂單資訊 可以摺疊*/}
@@ -22,110 +61,114 @@ function StepTwo() {
             <div className="row mt-5">
               <div className="col-md-12 col-24">
                 <div className="shipping-info ch-cont-14">
-                  {/* TODO: Form className='needs-validation ' 表單驗證*/}
-                  <div className="my-4">
-                    <div className="d-flex justify-content-between">
-                      <label className="form-label ">訂購人姓名</label>
-                      <label className="form-label text-primary ch-cont-12">
-                        必填
-                      </label>
-                    </div>
-                    <input
-                      type="text"
-                      className="form-control  "
-                      placeholder="name"
-                      required
-                    />
-                    <div class="invalid-feedback">
-                      Example invalid input group feedback
-                    </div>
-                  </div>
-                  <div className="my-4">
-                    <div className="d-flex justify-content-between">
-                      <label className="form-label ">連絡電話</label>
-                      <label className="form-label text-primary ch-cont-12">
-                        必填
-                      </label>
-                    </div>
-
-                    <input
-                      type="text"
-                      className="form-control "
-                      placeholder="phone"
-                      required
-                    />
-                    <div class="invalid-feedback">
-                      Example invalid input group feedback
-                    </div>
-                  </div>
-                  <div className="my-4">
-                    <label className="form-label ">取貨人姓名</label>
-                    <input
-                      type="text"
-                      className="form-control "
-                      placeholder="name"
-                    />
-                    <div className="form-check d-flex">
+                  <form>
+                    {/* TODO: Form className='needs-validation ' 表單驗證*/}
+                    <div className="my-4">
+                      <div className="d-flex justify-content-between">
+                        <label className="form-label ">訂購人姓名</label>
+                        <label className="form-label text-primary ch-cont-12">
+                          必填
+                        </label>
+                      </div>
                       <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
+                        type="text"
+                        className="form-control  "
+                        placeholder="name"
+                        required
                       />
-                      <label className="form-check-label ">同訂購人</label>
+                      <div className="invalid-feedback">
+                        Example invalid input group feedback
+                      </div>
                     </div>
-                  </div>
-                  <div className="my-4">
-                    <label
-                      for="exampleFormControlTextarea1 "
-                      className="form-label ch-cont-14"
-                    >
-                      訂單特殊備註
-                    </label>
-                    <textarea
-                      className="form-control textarea"
-                      id="exampleFormControlTextarea1"
-                      maxRows="5"
-                    ></textarea>
-                  </div>
-                  {/* TODO: map 光箱 */}
-                  <a
-                    type="button"
-                    className="btn btn-sm btn-outline-primary btn-block px-5 my-5 ch-title-14"
-                    href="#/"
-                  >
-                    選擇取貨門市
-                  </a>
+                    <div className="my-4">
+                      <div className="d-flex justify-content-between">
+                        <label className="form-label ">連絡電話</label>
+                        <label className="form-label text-primary ch-cont-12">
+                          必填
+                        </label>
+                      </div>
+
+                      <input
+                        type="text"
+                        className="form-control "
+                        placeholder="phone"
+                        required
+                      />
+                      <div className="invalid-feedback">
+                        Example invalid input group feedback
+                      </div>
+                    </div>
+                    <div className="my-4">
+                      <label className="form-label ">取貨人姓名</label>
+                      <input
+                        type="text"
+                        className="form-control "
+                        placeholder="name"
+                      />
+                      <div className="form-check d-flex">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          value=""
+                        />
+                        <label className="form-check-label ">同訂購人</label>
+                      </div>
+                    </div>
+                    <div className="my-4">
+                      <label
+                        htmlFor="exampleFormControlTextarea1 "
+                        className="form-label ch-cont-14"
+                      >
+                        訂單特殊備註
+                      </label>
+                      <textarea
+                        className="form-control textarea"
+                        id="exampleFormControlTextarea1"
+                      // maxRows="5"
+                      ></textarea>
+                    </div>
+                  </form>
                 </div>
               </div>
-              <CartSum className="d-none d-md-block" />
-              {/* 總計 */}
-              {/* <div className="col-md-12 d-none d-md-block">
-                <div className="summary-right  pt-3">
-                  <div className="row "></div>
-                  <div className="row print-time my-4">
-                    <div className="col-md-12 ch-cont-14">印製時間</div>
-                    <div className="col-md-8 text-right ch-cont-14">35分鐘</div>
-                  </div>
-                  <div className="row price my-4">
-                    <div className="col-md-12 ch-cont-14">訂單金額</div>
-                    <div className="col-md-8 text-right ch-cont-14">
-                      NT 3000
-                    </div>
-                  </div>
-                  <div className="row discount my-4">
-                    <div className="col-md-12 ch-cont-14">折抵金額</div>
-                    <div className="col-md-8 text-right ch-cont-14">NT 100</div>
-                  </div>
-
-                  <div className="row price my-4">
-                    <div className="col-md-12 ch-cont-14">結帳金額</div>
-                    <div className="col-md-8 text-right ch-cont-14">
-                      NT 2900
-                    </div>
-                  </div>
-                </div>
-              </div>*/}
+              {/* 右側 統計 summary */}
+              <CartSum
+                className="d-none d-md-block"
+              // style={{ position: 'fixed' }}
+              />
+              {/* ----summary-end */}
             </div>
+            {/* <div className="row"> */}
+            <div className="col-md-12">
+              {/* TODO: map 光箱 */}
+              {modal}
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-primary btn-block px-5 my-5 ch-title-14"
+                onClick={handleShow}
+              >
+                光箱 選擇取貨門市
+              </button>
+              {/*  ----------*/}
+              {/* 開關寫法 */}
+              {/* <a
+                type="button"
+                className="btn btn-sm btn-outline-primary btn-block px-5 my-5 ch-title-14"
+                href="#collapseExample"
+                data-toggle="collapse"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+                data-target="#collapseExample"
+              >
+                開關 選擇取貨門市
+              </a>
+              <div className="collapse" id="collapseExample">
+                <StepMap />
+              </div> */}
+              {/* -------------- */}
+            </div>
+            {/* -------------- */}
+            {/* </div> */}
 
             {/* 下一步 */}
             <div className="row  d-flex justify-content-center justify-content-md-end">
