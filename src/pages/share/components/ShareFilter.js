c10183bc97269170e0c69a180e8eb91b6ce8c490
Ryan Yang <ryanyang83@gmail.com>
2022-03-16 17:35:58 +0800

    Ryan 大改

--- src/pages/share/components/ShareFilter.js
+++ src/pages/share/components/ShareFilter.js
@@ -16,7 +16,7 @@ function ShareFilter(props) {
         {/* <div className={`${styles['waterfall-container']}`}></div> */}
         <div className={`mycontainer mb-5 classic`}>
           {/* 主要篩選條件區 */}
-          <div className="prod-filter">
+          <div className="prod-filter-ryan">
             {/* clean or cancel filter */}
             <div className="d-flex flex-column">
               <div className="filter-top justify-content-end">
