b1f80db5ef2c6dc6c1e1e4b09620802001158c90
kris <kris04090513@gmail.com>
2022-03-16 18:43:36 +0800

    ryan sharecolcontroller 死亡

---
+++ src/pages/share/components/ShareColController.js
@@ -0,0 +1,40 @@
+import styles from '../Share.module.scss';
+import '../../classic/index.scss';
+import '../ShareRangeInput.scss';
+
+import { ReactComponent as DeleteSm } from '../../../imgs/del.svg';
+
+function ShareColController(props) {
+  const { colControl, setColControl } = props;
+  return (
+    <>
+      <div
+        className={`${styles['col-controller-panel']} d-flex justify-content-center`}
+        style={!colControl ? { top: '-30%' } : { top: '0' }}
+      >
+        <div
+          className={`mt-5 col-8 d-flex flex-column align-items-center mr-5`}
+        >
+          <label htmlFor="col" className="en-title-18">
+            Column
+          </label>
+          <input type="range" min={2} max={6} id="col" defaultValue={4} />
+        </div>
+        <div className={`mt-5 col-8 d-flex flex-column align-items-center`}>
+          <label htmlFor="gap" className="en-title-18">
+            Gap
+          </label>
+          <input type="range" min={2} max={6} id="gap" defaultValue={4} />
+        </div>
+        <div className={`${styles['col-controller-del-btn']} `}>
+          <DeleteSm
+            className={`${styles['button-default-lg']}`}
+            onClick={() => setColControl(!colControl)}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ShareColController;
