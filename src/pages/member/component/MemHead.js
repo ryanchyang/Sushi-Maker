2620e6dc85fe77a4202a65e4b8918fbe0faca942
Joe Shih <injoe1001@gmail.com>
2022-03-20 23:25:52 +0800

    Joe 會員照片上傳完成

--- src/pages/member/component/MemHead.js
+++ src/pages/member/component/MemHead.js
@@ -13,13 +13,16 @@ function MemHead() {
     // TODO步驟2. 請參考WebApi 這支檔案裡的findMem這支, 只要把function名還有 ${BASE_URL}後的這串網址/member/api/find-member 改成自己的就好了(這串網址是你自訂的router的網址)
     findMem(mem_id).then(obj => {
       setMemData(obj[0]);
+      setImgSrc(
+        'http://localhost:3500/img/member' + '/' + obj[0].mem_photo_img_path
+      );
+      console.log(obj[0].mem_photo_img_path);
     }); //做到這裡如果node端API沒寫錯就會拿到你所需的資料了
   }, []);
 
   const handleSubmitUpload = () => {
     const fd = new FormData(document.form1);
     memDoUpload(fd).then(img => {
-      console.log(img.filename);
       setImgSrc('http://localhost:3500/img/member' + '/' + img.filename);
     });
   };
@@ -31,33 +34,35 @@ function MemHead() {
         onSubmit={e => e.preventDefault()}
         className="memPhotoArea col-md-6"
       >
-        <div
-          className="memPhoto"
-          onClick={e => document.querySelector('#avatar').click()}
-        >
+        <div className="memPhoto">
           <img className="memImg" src={imgSrc} alt="member-photo" />
         </div>
-
-        <input
-          type="text"
-          value={mem_id}
-          name="mem_id"
-          style={{ display: 'none' }}
-        />
 
         <div className="memName">
           <p className="ch-title-22">{memData ? memData.mem_name : ''}</p>
         </div>
         <button
           className="btn btn-primary primeal-btn-sm "
-          onClick={handleSubmitUpload}
+          onClick={e => document.querySelector('#avatar').click()}
         >
           點我上傳照片
         </button>
       </form>
 
       <form name="form1" style={{ display: 'none' }}>
-        <input type="file" id="avatar" name="avatar" accept="image/*" />
+        <input
+          type="file"
+          id="avatar"
+          name="avatar"
+          accept="image/*"
+          onChange={handleSubmitUpload}
+        />
+        <input
+          type="text"
+          value={mem_id}
+          name="mem_id"
+          style={{ display: 'none' }}
+        />
       </form>
 
       <div className="solgan col-md-4 mr-5">
