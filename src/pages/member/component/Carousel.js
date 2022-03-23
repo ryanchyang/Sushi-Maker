c1aaaa40d5effe58b62c59815c66bf8b795c9091
Joe Shih <injoe1001@gmail.com>
2022-03-24 00:38:22 +0800

    Joe 輪播牆試做

--- src/pages/member/component/Carousel.js
+++ src/pages/member/component/Carousel.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Hammer from 'hammerjs';
 
-const Carousel = ({ children, selectedIndex = 1 }) => {
+
+const Carousel = ({ selectedIndex = 1 }) => {
   const [status, setStatus] = useState(1);
   const [active, setActive] = useState(selectedIndex);
   const container = useRef(null);
   const SCREEN_WIDTH = window.screen.width;
+  const children = [
+    '/img/member/shareImg.png',
+    '/img/member/shareImg.png',
+    '/img/member/shareImg.png',
+    '/img/member/shareImg.png',
+  ];
 
   const setTransition = (offset = 0) => {
     function transitionend() {
@@ -34,9 +41,7 @@ const Carousel = ({ children, selectedIndex = 1 }) => {
     // 計算需要移動的距離並進行修改，這是切換的核心
     const distance = (1 - active) * SCREEN_WIDTH;
     container.current.addEventListener('transitionend', transitionend, false);
-    container.current.style.transform = `translate3d(${
-      distance + offset
-    }px,0,0)`;
+    container.current.style.transform = `translate3d(${distance+offset}px,0,0)`;
   };
 
   const handleChangeActive = number => {
@@ -51,14 +56,15 @@ const Carousel = ({ children, selectedIndex = 1 }) => {
   };
 
   useEffect(() => {
+    // setTransition();
     const manager = new Hammer(container.current);
     manager.add(new Hammer.Pan());
     manager.on('panend panmove', function (e) {
-      // 狀態在進行中時，不允許切換
-      if (status === 2) return;
-      // e.eventType 判斷當前狀態
-      // INPUT_MOVE 移動中
-      // INPUT_END 結束
+    // 狀態在進行中時，不允許切換
+    // if (status === 2) return;
+    // e.eventType 判斷當前狀態
+    // INPUT_MOVE 移動中
+    // INPUT_END 結束
       if (e.eventType === Hammer.INPUT_MOVE) {
         // 之前的offset參數的在此起到了作用，在手動滑動的時候並不是直接滑動到下一頁，只是跟隨手指進行偏移量改變
         setTransition(e.deltaX);
@@ -86,37 +92,54 @@ const Carousel = ({ children, selectedIndex = 1 }) => {
         manager.off('panend');
       };
     });
-  }, [status, active]);
+  }, [active, status]);
 
   // 為了演示是否成功，添加兩個按鈕來切換
   // 上一頁
   const handlePrev = () => {
     // 對臨界值進行處理
     handleChangeActive(active - 0.25);
+    // setActive(active - 0.25);
   };
   // 下一頁
   const handleNext = () => {
     // 對臨界值進行處理
     handleChangeActive(active + 0.25);
-  };
+    // setActive(active + 0.25);
+  }; //左
 
   return (
     <>
       <div className="divCarou">
-        <div ref={container} className="carouContainer">
-          <div className="carouImg mx-3">
+        <ul ref={container} className="carouContainer">
+          {children.map((v,i) => {
+            return (
+              <li className="carouImg mx-3">
+                <img className="Cimg" src={v} alt="" />
+              </li>
+            );
+          })}
+          {/* <li className="carouImg mx-3">
             <img className="Cimg" src="/img/member/shareImg.png" alt="" />
-          </div>
-          <div className="carouImg mx-3">
+          </li>
+          <li className="carouImg mx-3">
             <img src="/img/member/shareImg.png" className="Cimg" alt="" />
-          </div>
-          <div className="carouImg mx-3">
+          </li>
+          <li className="carouImg mx-3">
             <img src="/img/member/shareImg.png" className="Cimg" alt="" />
-          </div>
-          <div className="carouImg mx-3">
+          </li>
+          <li className="carouImg mx-3">
             <img src="/img/member/shareImg.png" className="Cimg" alt="" />
-          </div>
-        </div>
+          </li> */}
+        </ul>
+        {/* <div>
+          <button className="buttonLeft " onClick={handleNext}>
+            left
+          </button>
+          <button className="buttonRight " onClick={handlePrev}>
+            right
+          </button>
+        </div> */}
       </div>
       <button className="btn btn-primary primeal-btn-sm share-btn">
         觀看全部收藏
