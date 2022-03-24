2c07196b7fb025c664efa353d4cc0cbc8422553d
Joe Shih <injoe1001@gmail.com>
2022-03-24 20:51:29 +0800

    Joe 歷史訂單

--- src/pages/member/component/HistoryOrder.js
+++ src/pages/member/component/HistoryOrder.js
@@ -12,53 +12,73 @@ const HistoryOrder = () => {
   const [historyOrderInfo, setHistoryOrderInfo] = useState('');
   const [clickToggle, setClickToggle] = useState(false);
   const mem_id = localStorage.getItem('mem_id');
+  const [index, setIndex] = useState('');
 
-  const handleClickToggle = () => {
+  const handleClickToggle = e => {
     if (clickToggle === true) {
       setClickToggle(false);
     } else {
       setClickToggle(true);
     }
+    setIndex(e.target.dataset.value);
+
+    console.log(index);
   };
 
   useEffect(() => {
     orderInfo(mem_id).then(obj => {
-      console.log(obj);
       setHistoryOrderInfo(obj);
     });
   }, []);
 
   return (
     <div className="row orderDetailRow">
-      <table className="table col-md-20">
+      <table className="table col-md-20 ch-title-16">
         <thead>
           <tr>
-            <th scope="col">訂單編號</th>
-            <th scope="col">購買日期</th>
-            <th scope="col">訂單狀態</th>
-            <th scope="col">總價</th>
-            <th scope="col">印製時間</th>
-            <th scope="col"></th>
+            <th scope="col" className="col-4">
+              訂單編號
+            </th>
+            <th scope="col" className="col-4">
+              購買日期
+            </th>
+            <th scope="col" className="col-4">
+              訂單狀態
+            </th>
+            <th scope="col" className="col-4">
+              總價
+            </th>
+            <th scope="col" className="col-4">
+              印製時間
+            </th>
+            <th scope="col" className="col-4"></th>
           </tr>
         </thead>
       </table>
       {historyOrderInfo &&
-        historyOrderInfo.newData2.map(data => {
+        historyOrderInfo.newData2.map((data, i) => {
           return (
             <>
-              <table className="table col-md-20">
+              <table className="table col-md-20 ch-cont-16">
                 <tbody>
                   <tr>
-                    <td>{data.order_num}</td>
-                    <td>{data.cart_checkout_date}</td>
-                    <td>{data.cart_status}</td>
-                    <td>NT$ {data.cart_value} 元</td>
-                    <td>{data.cart_total_print_time} 分鐘</td>
+                    <td className="col-4">{data.order_num}</td>
+                    <td className="col-4">{data.cart_checkout_date}</td>
+                    <td className="col-4">{data.cart_status}</td>
+                    <td className="col-4">NT$ {data.cart_value} 元</td>
+                    <td className="col-4">{data.cart_total_print_time} 分鐘</td>
                     <td
+                      className="col-4"
                       onClick={handleClickToggle}
                       style={{ cursor: 'pointer' }}
+                      data-value={i}
                     >
-                      交易明細 {clickToggle ? <SelectClose /> : <SelectOpen />}
+                      交易明細
+                      {clickToggle && index == i ? (
+                        <SelectClose />
+                      ) : (
+                        <SelectOpen />
+                      )}
                     </td>
                   </tr>
                 </tbody>
@@ -67,12 +87,17 @@ const HistoryOrder = () => {
               <div
                 className="col-md-20"
                 style={
-                  clickToggle
+                  clickToggle && index == i
                     ? { display: 'block', transition: '1s' }
                     : { display: 'none', transition: '1s' }
                 }
               >
-                <div style={{}}>
+                <div
+                  style={{
+                    backgroundColor: '#ffffff',
+                    padding: '2%',
+                  }}
+                >
                   {historyOrderInfo.newData &&
                     historyOrderInfo.newData
                       .filter(d => d.cart_id === data.cart_id)
@@ -83,25 +108,30 @@ const HistoryOrder = () => {
                               style={{
                                 display: 'flex',
                                 justifyContent: 'space-between',
-                                marginBottom: '5%',
+                                marginBottom: '3%',
                                 textAlign: 'left',
-                
                               }}
                             >
-                              <div style={{ border: '1px solid red' }} className="col-4">
-                                圖片
+                              <div className="col-4">
+                                <img
+                                  style={{ width: '100px' }}
+                                  src={
+                                    'http://localhost:3500' +
+                                    info.c_prod_img_path
+                                  }
+                                />
                               </div>
-                              <div style={{ border: '1px solid red' }} className="col-4">
+                              <div className="col-4 ch-cont-14">
                                 {info.c_prod_ch_name}
                               </div>
-                              <div style={{ border: '1px solid red' }} className="col-4">
-                                件數
+                              <div className="col-4 ch-cont-14">
+                                共{info.orders_amount}件
                               </div>
-                              <div style={{ border: '1px solid red' }} className="col-4">
-                                金額
+                              <div className="col-4 ch-cont-14">
+                                NT {info.c_prod_value}元/件
                               </div>
-                              <div style={{ border: '1px solid red' }} className="col-4">
-                                印製時間
+                              <div className="col-4 ch-cont-14">
+                                {info.orders_print_time / 60}分鐘/件
                               </div>
                             </div>
                           </>
