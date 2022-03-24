1d6c29319777c40af998c5ca63d2373229e492ce
lestellevolano <lestellevolano3@gmail.com>
2022-03-24 16:41:57 +0800

    La-stepone

--- src/pages/setorder/StepOne.js
+++ src/pages/setorder/StepOne.js
@@ -1,4 +1,5 @@
 import { Header, Title, AsideLeft, AsideRight, Footer } from '../layout/Layout';
+import { Link, useHistory } from 'react-router-dom';
 import './step.scss';
 import './../../styles/global.scss';
 
@@ -14,25 +15,30 @@ function StepOne() {
       <div style={{ display: 'flex' }}>
         <AsideLeft />
         <div style={{ width: '100%' }}>
-          <Title title={'JUST FOR YOU-one'} />
-          <br />
+          <Title title={'JUST FOR YOU'} />
           <div className="step">
             <div className="mycontainer min-hi">
               <div className="set-all-content">
-                <div className="set-title col-12">
-                  <div className="step-mob-title set-text-center">
-                    讓我們來幫您規劃您的專屬菜單
+                <div className="set-title col-12 set-title-0">
+                  <div className="step-mob-title set-text-center set-title-1">
+                    規劃您的專屬菜單
                   </div>
                 </div>
-                <div className="set-question-box">
+                <div className="set-question-box step-one-question-box">
                   <div className="ch-cont-18 set-text-center set-content">
                     <p>我們將根據您的偏好推薦餐點</p>
                     <p>但您仍然可以修改完整套餐</p>
                   </div>
-                  <button class="ch-title-22 set-button dark-red">
-                    下一步
-                  </button>
-                 
+                  <div className="step-reset-enter-btn-all">
+                    <Link to="./stepthree">
+                      <button class="ch-title-22 step-reset-btn">返回</button>
+                    </Link>
+                    <Link to="./steptwo">
+                      <button class="ch-title-22 step-reset-btn-red">
+                        確定
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
