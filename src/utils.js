e26af74b9239ffdf7fac3ee4c1df2b8e0851c46a
Sheng <yso9197ce@hotmail.com>
2022-03-26 10:44:16 +0800

    Sheng: add utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+import config from './Config';
 const TOKEN_NAME = 'token';
 const MEM_ID = 'mem_id';
 
@@ -16,4 +17,22 @@ export const setMemId = mem_id => {
 //取memID
 export const getMemId = () => {
   return localStorage.getItem(MEM_ID);
-}
+};
+
+//取購物車資料
+export const getCart = async () => {
+  const isLogin = localStorage.getItem(MEM_ID) !== null;
+  if (isLogin) {
+    //會員有登入
+    const res = await fetch(config.GET_CUR_CART + localStorage.getItem(MEM_ID));
+    const obj = await res.json();
+    return obj;
+  } else {
+    //會員無登入
+    return {
+      hasCurCart: false,
+      cartid: 0,
+      cartCount: 0,
+    };
+  }
+};
