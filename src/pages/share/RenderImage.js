732729674545cd65c5566e4a14ee29910e64c223
Ryan Yang <ryanyang83@gmail.com>
2022-03-31 18:06:53 +0800

    Ryan three test

--- src/pages/share/RenderImage.js
+++ src/pages/share/RenderImage.js
@@ -1,19 +1,16 @@
 import React, { useMemo, useEffect, useState } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { RoundedBox, useTexture } from '@react-three/drei';
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { useTexture } from '@react-three/drei';
+// import { TextureLoader } from 'three/src/loaders/TextureLoader';
 // All textures are CC0 textures from: https://cc0textures.com/
 
 function RenderImage(props) {
-  const [egg, eggNormal] = useTexture(['/img/egg.jpg', '/img/egg-normal.jpg']);
-  const [rice, riceNormal] = useTexture([
-    '/img/rice2.jpg',
-    '/img/rice-normal.jpg',
-  ]);
-  const [meat, meatNormal] = useTexture([
-    '/img/meat.jpg',
-    '/img/meat-normal.jpg',
-  ]);
+  useTexture(['/img/egg.jpg', '/img/egg-normal.jpg']);
+  useTexture(['/img/rice2.jpg', '/img/rice-normal.jpg']);
+  useTexture(['/img/meat.jpg', '/img/meat-normal.jpg']);
+  useTexture(['/img/bottarga.png', '/img/bottarga-normal.png']);
+  useTexture(['/img/salmon.jpg', '/img/salmon-normal.jpg']);
+  useTexture(['/img/unknown.jpg', '/img/unknown-normal.jpg']);
+  useTexture(['/img/Pumpkin.jpg', '/img/Pumpkin-normal.jpg']);
 
   return <></>;
 }
--- src/pages/share/Scene.js
+++ src/pages/share/Scene.js
@@ -1,17 +1,47 @@
-import React, { Suspense, useMemo, useEffect, useState } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+// import * as THREE from 'three';
+import React, { useEffect, useState, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 import { RoundedBox, useTexture } from '@react-three/drei';
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
+// import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { useSpring } from '@react-spring/core';
+import { a } from '@react-spring/three';
 // All textures are CC0 textures from: https://cc0textures.com/
 const name = type => `PavingStones092_1K_${type}.jpg`;
 
 function Scene(props) {
-  const { sushi, index, sushiGroup, setSushiGroup } = props;
-  const { map, normalMap } = sushi;
-  console.log(sushi);
-
+  const { sushi, index, sushiGroup, setSushiGroup, isChange, setIsChange } =
+    props;
+  const { map, normalMap, height, alt, fixIndex } = sushi;
+  const [active, setActive] = useState(0);
+  const mesh = useRef();
   const [img, normalImg] = useTexture([map, normalMap]);
 
+  // interpolate values from common spring
+  const { position } = useSpring({
+    from: { position: [0, fixIndex / 2 + alt + 0.5, 0] },
+    to: async next => {
+      if (active) {
+        await next({ position: [0, fixIndex / 2 + alt, 0] });
+      }
+    },
+    config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
+  });
+
+  // img.wrapS = THREE.RepeatWrapping; //調整材質的大小
+  // img.wrapT = THREE.RepeatWrapping;
+  // img.repeat.set(1, 1); // 數字越大 單張圖片材質本身越小 重複的次數越多
+  // normalImg.wrapS = THREE.RepeatWrapping;
+  // normalImg.wrapT = THREE.RepeatWrapping;
+  // normalImg.repeat.set(1, 1);
+
+  useFrame(state => {
+    const t = state.clock.getElapsedTime();
+    // mesh.current.rotation.z = -0.2 - (1 + Math.sin(t / 1.5)) / 20;
+    // mesh.current.rotation.x = Math.cos(t / 4) / 8;
+    mesh.current.rotation.y = Math.sin(t / 4) / 8;
+    // mesh.current.position.y = (1 + Math.sin(t / 1.5)) / 10 + fixIndex / 2 + alt; //Y軸上下移動
+  });
+  // console.log(mesh.current?.position.y);
   // const [img, normalImg] = useLoader(TextureLoader, [map, normalMap]);
   // const [
   //   colorMap,
@@ -27,17 +57,27 @@ function Scene(props) {
   //   name("AmbientOcclusion")
   // ]);
 
-  return 1 ? (
+  const changeHeightIndexHandler = () => {
+    setIsChange([...isChange, index]);
+  };
+
+  useEffect(() => {
+    if (index === sushiGroup.length - 1) setActive(Number(!active));
+  }, []);
+
+  return (
     <>
-      <mesh
-        position={[0, index / 2, 0]}
+      <a.mesh
+        ref={mesh}
+        // position={[0, fixIndex / 2 + alt, 0]}
+        position={position}
         onClick={() => {
-          console.log(index);
-          setSushiGroup(() => {
-            let returnGroup = [...sushiGroup];
-            returnGroup.splice(index, 1);
-            return returnGroup;
-          });
+          changeHeightIndexHandler();
+          // setSushiGroup(() => {
+          //   let returnGroup = [...sushiGroup];
+          //   returnGroup.splice(index, 1);
+          //   return returnGroup;
+          // });
         }}
       >
         {/* Width and height segments for displacementMap */}
@@ -47,17 +87,16 @@ function Scene(props) {
           map={colorMap}
           normalMap={normalMap}
         /> */}
-        <RoundedBox args={[1, 0.5, 1]} radius={0.05} smoothness={4}>
+
+        <RoundedBox args={[1, height, 1]} radius={0.04} smoothness={4}>
           <meshStandardMaterial
             bumpScale={0.05}
             map={img}
             normalMap={normalImg}
           />
         </RoundedBox>
-      </mesh>
+      </a.mesh>
     </>
-  ) : (
-    ''
   );
 }
 
